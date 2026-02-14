9fd7eedc0a6cfda78b58b7f098d2cadefbe5350e
Shrawan <shrawankick@gmail.com>
2026-02-14 23:26:58 +0530

    Add multiple Python scripts for chai preparation and data types demonstration

---
+++ bootcamp1/02_dataTypes/c2.py
@@ -0,0 +1,14 @@
+is_sunny = True
+have_umbrella = False
+ 
+# Is it not sunny today?
+print("Is it not sunny today?", not is_sunny)
+
+# Do you not have an umbrella?
+print("Do you not have an umbrella?", not have_umbrella)
+
+# Should you go for a walk if it's sunny and you don't need an umbrella?
+print("Should you go for a walk if it's sunny and you don't need an umbrella?", is_sunny and not have_umbrella)
+
+# Should you go for a walk if it's sunny or if you have an umbrella?
+print("Should you go for a walk if it's sunny or if you have an umbrella?", is_sunny or have_umbrella)
